b67a90339215b2605c935363515a26f8cf315006
alexfra <alexandre.donazzan@gmail.com>
2016-09-28 14:54:29 +0200

    ns push alexandre

--- src/java/greta/speedymarket/services/TbCategorieService.java
+++ src/java/greta/speedymarket/services/TbCategorieService.java
@@ -7,19 +7,35 @@ package greta.speedymarket.services;
 
 import greta.speedymarket.dao.TbCategorieDAO;
 import greta.speedymarket.model.TbCategorie;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
 
 @ManagedBean(name = "TbCategorieService")
 @ViewScoped
 public class TbCategorieService {
 
     private TbCategorie selectedCategorie;
+    private TreeNode root;
+    private TreeNode selectedNode;
 
+    public TreeNode getSelectedNode() {
+        return selectedNode;
+    }
+
+    public void setSelectedNode(TreeNode selectedNode) {
+        this.selectedNode = selectedNode;
+    }
+
+    public TbCategorie getSelectedNodeCategorie() {
+        return (TbCategorie) getSelectedNode().getData();
+    }
+    
     public TbCategorie getSelectedCategorie() {
         return this.selectedCategorie;
-
     }
 
     public void setSelectedCategorie(TbCategorie selectedCategorie) {
@@ -51,4 +67,40 @@ public class TbCategorieService {
         TbCategorieDAO tbCategorieDAO = new TbCategorieDAO();
         return tbCategorieDAO.findAll();
     }
+
+    public List<TbCategorie> loadCategorieParentes() {
+        List<TbCategorie> categories = new ArrayList<>();
+
+        for (TbCategorie category : loadCategories()) {
+            if (category.getTbCategorie() == null) {
+                categories.add(category);
+            }
+        }
+        return categories;
+    }
+
+    public TreeNode getRoot() {
+        root = new DefaultTreeNode();
+
+        for (TbCategorie categorie : loadCategorieParentes()) {
+            TreeNode node = new DefaultTreeNode(categorie, root);
+
+            createCategorieTreeNode(categorie, node);
+
+        }
+        return root;
+    }
+
+    private void createCategorieTreeNode(TbCategorie categorie, TreeNode nodeParent) {
+        for (Object category : categorie.getTbCategories()) {
+            TbCategorie cat = (TbCategorie) category;
+            TreeNode node = new DefaultTreeNode(category, nodeParent);
+
+            createCategorieTreeNode(cat, node);
+        }
+    }
+
+    
+
+    
 }
--- src/java/greta/speedymarket/services/TbCommandeService.java
+++ src/java/greta/speedymarket/services/TbCommandeService.java
@@ -1,5 +1,5 @@
-
 package greta.speedymarket.services;
+
 import greta.speedymarket.dao.TbCommandeDAO;
 import greta.speedymarket.model.TbCommande;
 import java.util.List;
@@ -10,46 +10,43 @@ import javax.faces.bean.ViewScoped;
  *
  * @author Stagiaires
  */
-@ManagedBean(name="TbCommandeService")
+@ManagedBean(name = "TbCommandeService")
 @ViewScoped
 public class TbCommandeService {
-private TbCommande selectedCommande;
-public TbCommande getSelectedCommande() {
-return this.selectedCommande;
 
-}
+    private TbCommande selectedCommande;
 
-public void setSelectedCommande(TbCommande selectedCommande) {
-this.selectedCommande = selectedCommande;
-}
+    public TbCommande getSelectedCommande() {
+        return this.selectedCommande;
 
-public void createCommande() {
-TbCommande newCommande = new TbCommande();
-TbCommandeDAO tbCommandeDAO = new TbCommandeDAO();
-tbCommandeDAO.save(newCommande);
-}
+    }
 
+    public void setSelectedCommande(TbCommande selectedCommande) {
+        this.selectedCommande = selectedCommande;
+    }
 
+    public void createCommande() {
+        TbCommande newCommande = new TbCommande();
+        TbCommandeDAO tbCommandeDAO = new TbCommandeDAO();
+        tbCommandeDAO.save(newCommande);
+    }
 
-public void saveCommande(TbCommande commande) {
-if (commande != null) {
-TbCommandeDAO tbCommandeDAO = new TbCommandeDAO();
-tbCommandeDAO.update(commande);
-}
-}
+    public void saveCommande(TbCommande commande) {
+        if (commande != null) {
+            TbCommandeDAO tbCommandeDAO = new TbCommandeDAO();
+            tbCommandeDAO.update(commande);
+        }
+    }
 
+    public void deleteCommande(TbCommande commande) {
+        if (commande != null) {
+            TbCommandeDAO tbCommandeDAO = new TbCommandeDAO();
+            tbCommandeDAO.remove(commande);
+        }
+    }
 
-public void deleteCommande(TbCommande commande) {
-if (commande != null) {
-TbCommandeDAO tbCommandeDAO = new TbCommandeDAO();
-tbCommandeDAO.remove(commande);
-}
-}
-
-
-
-public List<TbCommande> loadCommandes() {
-TbCommandeDAO tbCommandeDAO = new TbCommandeDAO();
-return tbCommandeDAO.findAll();
-}
+    public List<TbCommande> loadCommandes() {
+        TbCommandeDAO tbCommandeDAO = new TbCommandeDAO();
+        return tbCommandeDAO.findAll();
+    }
 }
